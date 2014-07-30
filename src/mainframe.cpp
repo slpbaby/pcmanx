46e24da04e6ca9ca150e351229db787907d480d3
Jim Huang <jserv@0xlab.org>
2014-07-30 14:58:30 +0800

    Add myself as authors

--- src/mainframe.cpp
+++ src/mainframe.cpp
@@ -950,6 +950,7 @@ void CMainFrame::OnShortcutList(GtkMenuItem* mitem UNUSED, CMainFrame* _this)
 void CMainFrame::OnAbout(GtkMenuItem* mitem UNUSED, CMainFrame* _this)
 {
 	char* authors = _(
+			"Somebody stronger than PCMan\n"
 			"Hong Jen Yee (Main developer) <pcman.tw@gmail.com>\n"
 			"Jim Huang (Developer) <jserv.tw@gmail.com>\n"
 			"Kan-Ru Chen (Developer) <kanru@kanru.info>\n"
